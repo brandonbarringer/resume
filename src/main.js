e77df392a84b7fa665e929ba08a3174de2ab40bd
Brandon Barringer <barringerbrandon@gmail.com>
2019-08-05 08:08:32 -0700

    creates base structure

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import VueFlex from "vue-flex";
+import "vue-flex/dist/vue-flex.css";
 
+
+Vue.use(VueFlex);
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+<main>
+	<div class="container">
+		<aside class="profile">
+			<img src="./../../public/img/brandon-barringer.jpg" alt="Brandon Barringer" class="profile__img">
+			<div class="content">
+				<h1 class="content__headline headline">Brandon Barringer</h1>
+				<p class="content__subheadline subheadline">Creative Designer &amp; Developer with a knack for problem solving, creativity and process efficiancy</p>
+				<div class="content__description">
+					<p>Hi. I'm Brandon. I've been a designer and developer since my first introduction to graphic communications in high school in 2004.</p>
+					<p>I'm a huge nerd that is constantly looking for the best solution to any problem in life and at work, like the most efficient route to take home, the perfect way to stack a sandwhich and the actual solution to <code>8&#247;2(2+2)</code> <small><em> It's 16 by the way</em></small></p>
+					<p>Anyway, if you're interested in having a chat just..</p>
+				</div>
+				<a href="#!" class="button">Contact Me</a>
+			</div>
+		</aside>
+		<section class="history">
+			<h1 class="history__headline headline">History</h1>
+			<h2 class="history__section-headline secondary-headline">Work</h2>
+			<article class="card">
+				<div class="card__container">
+					<div class="card__date-container">
+						<time class="card__date card__date--start">August 15<sup>th</sup> 2013</time>
+						<time class="card__date card__date--end">Current</time>
+					</div>
+					<h1 class="card__title">Noble Studios</h1>
+					<p class="card__subheadline subheadline">Creative Designer</p>
+					<p class="card__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae pariatur alias blanditiis, minus beatae labore. Nemo illo quae mollitia sequi architecto ad dignissimos aliquid, ut dolor cum incidunt alias magnam.</p>
+				</div>
+			</article>
+			<article class="card">
+				<div class="card__container">
+					<div class="card__date-container">
+						<time class="card__date card__date--start">June 15<sup>th</sup> 2013</time>
+						<time class="card__date card__date--end">August 15<sup>th</sup> 2013</time>
+					</div>
+					<h1 class="card__title">Noble Studios</h1>
+					<p class="card__subheadline subheadline">Design Intern</p>
+					<p class="card__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae pariatur alias blanditiis, minus beatae labore. Nemo illo quae mollitia sequi architecto ad dignissimos aliquid, ut dolor cum incidunt alias magnam.</p>
+				</div>
+			</article>
+			<h2 class="history__section-headline secondary-headline">Education</h2>
+			<h2 class="history__section-headline secondary-headline">Notable Acheivements</h2>
+
+		</section>
+		<section class="skills">
+			<h1 class="skills__headline headline">Skills</h1>
+		</section>
+	</div>
+</main>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
+
 </script>
+
+<style lang="sass">
+	@import './public/sass/variables.sass'
+	@import './public/sass/profile.sass'
+	@import './public/sass/history.sass'
+	@import './public/sass/skills.sass'
+</style>
