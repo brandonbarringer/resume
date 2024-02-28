fd951edbe56621b41bd14445ec438f9035ce6fa8
Brandon Barringer <barringerbrandon@gmail.com>
2024-02-28 08:04:08 -0600

    swaps node-sass for sass, updates framework to include astro

--- src/components/FrameworkChart.vue
+++ src/components/FrameworkChart.vue
@@ -36,17 +36,17 @@
 		methods: {
 			fillData() {
 				this.dataCollection = {
-					labels: ['Vue', 'Angular', 'React', 'Jekyll', 'Svelte'],
+					labels: ['Vue', 'Angular', 'React', 'Jekyll', 'Svelte', 'Astro'],
 					datasets: [{
 						label: 'Languages',
-						data: [80, 20, 80, 40, 70],
+						data: [80, 20, 90, 40, 70, 50],
 						backgroundColor: [
 						'rgba(236,64,122 ,.1)',
 						'rgba(41,182,246 ,.1)',
 						'rgba(156,204,101 ,.1)',
 						'rgba(255,112,67 ,.1)',
 						'rgba(138,23,224 ,.1)',
-						'rgba(138,23,224 ,.1)'
+						'rgba(237, 221, 26 ,.1)'
 						],
 						borderColor: [
 						'rgba(236,64,122 ,.5)',
@@ -54,7 +54,7 @@
 						'rgba(156,204,101 ,.5)',
 						'rgba(255,112,67 ,.5)',
 						'rgba(138,23,224 ,.5)',
-						'rgba(138,23,224 ,.6)'
+						'rgba(237, 221, 26 ,.6)'
 						]
 					}]
 				}
